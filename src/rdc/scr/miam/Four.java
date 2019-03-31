3fe3322cce45150428e69112c7de002f16a63190
youri <youridaydaykmukaz@gmail.com>
2019-03-31 08:22:30 +0100

    relation d'association entre Four et Aliment

--- src/rdc/scr/miam/Four.java
+++ src/rdc/scr/miam/Four.java
@@ -1,16 +1,20 @@
 package rdc.scr.miam;
 
+import rdc.scr.miam.*;
+
 public class Four {
     public  int puissance ;
 
     public int capacite;
 
-    public void cuire(){
-        System.out.println("Je cuis un aliment");
+    public void cuire(Aliment aliment){
+        System.out.println("Je cuis un aliment" +aliment.nom);
 
         System.out.println("avec ma capaciter de " + capacite+" litres");
 
         System.out.println("et ma puissance de "+ puissance + " degres");
+
+        aliment.estCuit = true;
 
     }
 }
