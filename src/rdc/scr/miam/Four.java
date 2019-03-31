46bfb014abeff254de56782209d954d4a6678647
youri <youridaydaykmukaz@gmail.com>
2019-03-31 07:55:17 +0100

    ajoute de la class Four dans miam: -attribut -methode

---
+++ src/rdc/scr/miam/Four.java
@@ -0,0 +1,15 @@
+package rdc.scr.miam;
+
+public class Four {
+    int puissance ;
+    int capacite;
+
+    public void cuire(){
+        System.out.println("Je cuis un aliment");
+
+        System.out.println("avec ma capaciter de " + capacite+"litres");
+
+        System.out.println("et ma puissance de "+ puissance + "degres");
+
+    }
+}
